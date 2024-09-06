4c1ad14d87107e6a44d5c3dbeaa073a7f0829a7e
verysillycat <cortex@duck.com>
2024-09-06 06:12:46 -0600

    ags: forgot one commit to sync

--- .config/ags/modules/bar/normal/workspaces_hyprland.js
+++ .config/ags/modules/bar/normal/workspaces_hyprland.js
@@ -15,10 +15,33 @@ const dummyWs = Box({ className: "bar-ws" }); // Not shown. Only for getting siz
 const dummyActiveWs = Box({ className: "bar-ws bar-ws-active" }); // Not shown. Only for getting size props
 const dummyOccupiedWs = Box({ className: "bar-ws bar-ws-occupied" }); // Not shown. Only for getting size props
 
+const mix = (value1, value2, perc) => {
+  return value1 * perc + value2 * (1 - perc);
+};
+
+const getFontWeightName = (weight) => {
+  switch (weight) {
+    case Pango.Weight.ULTRA_LIGHT:
+      return 'UltraLight';
+    case Pango.Weight.LIGHT:
+      return 'Light';
+    case Pango.Weight.NORMAL:
+      return 'Normal';
+    case Pango.Weight.BOLD:
+      return 'Bold';
+    case Pango.Weight.ULTRA_BOLD:
+      return 'UltraBold';
+    case Pango.Weight.HEAVY:
+      return 'Heavy';
+    default:
+      return 'Normal';
+  }
+};
+
 // Font size = workspace id
 const WorkspaceContents = (count = 10) => {
   return DrawingArea({
-    // css: `transition: 300ms cubic-bezier(0.1, 1, 0, 1);`,
+    className: 'bar-ws-container',
     attribute: {
       initialized: false,
       workspaceMask: 0,
@@ -95,6 +118,10 @@ const WorkspaceContents = (count = 10) => {
               "font-family",
               Gtk.StateFlags.NORMAL,
             );
+            const workspaceFontWeight = workspaceStyleContext.get_property(
+              "font-weight",
+              Gtk.StateFlags.NORMAL,
+            );
             const wsbg = workspaceStyleContext.get_property(
               "background-color",
               Gtk.StateFlags.NORMAL,
@@ -139,7 +166,7 @@ const WorkspaceContents = (count = 10) => {
             // Font
             const layout = PangoCairo.create_layout(cr);
             const fontDesc = Pango.font_description_from_string(
-              `${workspaceFontFamily[0]} ${workspaceFontSize}`,
+              `${workspaceFontFamily[0]} ${getFontWeightName(workspaceFontWeight)} ${workspaceFontSize}`,
             );
             layout.set_font_description(fontDesc);
             cr.setAntialias(Cairo.Antialias.BEST);
@@ -147,74 +174,25 @@ const WorkspaceContents = (count = 10) => {
             layout.set_text("0".repeat(count.toString().length), -1);
             const [layoutWidth, layoutHeight] = layout.get_pixel_size();
             const indicatorRadius =
-              (Math.max(layoutWidth, layoutHeight) / 2) * 1.2; // a bit smaller than sqrt(2)*radius
+              (Math.max(layoutWidth, layoutHeight) / 2) * 1.15; // smaller than sqrt(2)*radius
             const indicatorGap = workspaceRadius - indicatorRadius;
 
             // Draw workspace numbers
             for (let i = 1; i <= count; i++) {
-              if (area.attribute.workspaceMask & (1 << i)) {
-                // Draw bg highlight
-                cr.setSourceRGBA(
-                  occupiedbg.red,
-                  occupiedbg.green,
-                  occupiedbg.blue,
-                  occupiedbg.alpha,
-                );
-                const wsCenterX = -workspaceRadius + workspaceDiameter * i;
-                const wsCenterY = height / 2;
-                if (!(area.attribute.workspaceMask & (1 << (i - 1)))) {
-                  // Left
-                  cr.arc(
-                    wsCenterX,
-                    wsCenterY,
-                    workspaceRadius,
-                    0.5 * Math.PI,
-                    1.5 * Math.PI,
-                  );
-                  cr.fill();
-                } else {
-                  cr.rectangle(
-                    wsCenterX - workspaceRadius,
-                    wsCenterY - workspaceRadius,
-                    workspaceRadius,
-                    workspaceRadius * 2,
-                  );
-                  cr.fill();
-                }
-                if (!(area.attribute.workspaceMask & (1 << (i + 1)))) {
-                  // Right
-                  cr.arc(
-                    wsCenterX,
-                    wsCenterY,
-                    workspaceRadius,
-                    -0.5 * Math.PI,
-                    0.5 * Math.PI,
-                  );
-                  cr.fill();
-                } else {
-                  cr.rectangle(
-                    wsCenterX,
-                    wsCenterY - workspaceRadius,
-                    workspaceRadius,
-                    workspaceRadius * 2,
-                  );
-                  cr.fill();
-                }
-
-                // Set color for text
-                cr.setSourceRGBA(
-                  occupiedfg.red,
-                  occupiedfg.green,
-                  occupiedfg.blue,
-                  occupiedfg.alpha,
-                );
-              } else
-                cr.setSourceRGBA(wsfg.red, wsfg.green, wsfg.blue, wsfg.alpha);
+              const inactivecolors = area.attribute.workspaceMask & (1 << i) ? occupiedfg : wsfg;
+              if (i == activeWs) {
+                cr.setSourceRGBA(activefg.red, activefg.green, activefg.blue, activefg.alpha);
+              } else if ((i == Math.floor(activeWs) && Hyprland.active.workspace.id < activeWs) || (i == Math.ceil(activeWs) && Hyprland.active.workspace.id > activeWs)) {
+                cr.setSourceRGBA(mix(activefg.red, inactivecolors.red, 1 - Math.abs(activeWs - i)), mix(activefg.green, inactivecolors.green, 1 - Math.abs(activeWs - i)), mix(activefg.blue, inactivecolors.blue, 1 - Math.abs(activeWs - i)), activefg.alpha);
+              } else if ((i == Math.floor(activeWs) && Hyprland.active.workspace.id > activeWs) || (i == Math.ceil(activeWs) && Hyprland.active.workspace.id < activeWs)) {
+                cr.setSourceRGBA(mix(activefg.red, inactivecolors.red, 1 - Math.abs(activeWs - i)), mix(activefg.green, inactivecolors.green, 1 - Math.abs(activeWs - i)), mix(activefg.blue, inactivecolors.blue, 1 - Math.abs(activeWs - i)), activefg.alpha);
+              } else {
+                cr.setSourceRGBA(inactivecolors.red, inactivecolors.green, inactivecolors.blue, inactivecolors.alpha);
+              }
 
               layout.set_text(`${i + offset}`, -1);
               const [layoutWidth, layoutHeight] = layout.get_pixel_size();
-              const x =
-                -workspaceRadius + workspaceDiameter * i - layoutWidth / 2;
+              const x = -workspaceRadius + workspaceDiameter * i - layoutWidth / 2;
               const y = (height - layoutHeight) / 2;
               cr.moveTo(x, y);
               PangoCairo.show_layout(cr, layout);
