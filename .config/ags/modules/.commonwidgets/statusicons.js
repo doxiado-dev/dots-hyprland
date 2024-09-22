eda4d2ee1abeca626d0e38e7a61054af72e1fbda
Cortex <126973723+verysillycat@users.noreply.github.com>
2024-09-21 23:09:02 -0600

    ags(statusicons): weatherIconOnlyOnConnect improvements

--- .config/ags/modules/.commonwidgets/statusicons.js
+++ .config/ags/modules/.commonwidgets/statusicons.js
@@ -474,6 +474,34 @@ const WeatherWidget = () => {
     self.tooltipText = weatherDesc;
   };
 
+  const fetchWeatherData = (self, city) => {
+    const WEATHER_CACHE_PATH = WEATHER_CACHE_FOLDER + "/wttr.in.txt";
+    Utils.execAsync(`curl https://wttr.in/${city.replace(/ /g, "%20")}?format=j1`)
+      .then((output) => {
+        const weather = JSON.parse(output);
+        Utils.writeFile(JSON.stringify(weather), WEATHER_CACHE_PATH).catch(print);
+        const weatherCode = weather.current_condition[0].weatherCode;
+        const weatherDesc = weather.current_condition[0].weatherDesc[0].value;
+        const temperature = weather.current_condition[0][`temp_${userOptions.weather.preferredUnit}`];
+        const feelsLike = weather.current_condition[0][`FeelsLike${userOptions.weather.preferredUnit}`];
+        const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
+        updateWeatherDisplay(self, weather, weatherSymbol, temperature, feelsLike, weatherDesc);
+      })
+      .catch((err) => {
+        try {
+          const weather = JSON.parse(Utils.readFile(WEATHER_CACHE_PATH));
+          const weatherCode = weather.current_condition[0].weatherCode;
+          const weatherDesc = weather.current_condition[0].weatherDesc[0].value;
+          const temperature = weather.current_condition[0][`temp_${userOptions.weather.preferredUnit}`];
+          const feelsLike = weather.current_condition[0][`FeelsLike${userOptions.weather.preferredUnit}`];
+          const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
+          updateWeatherDisplay(self, weather, weatherSymbol, temperature, feelsLike, weatherDesc);
+        } catch (err) {
+          print(err);
+        }
+      });
+  };
+
   return Widget.Box({
     hexpand: userOptions.weather.spacing,
     hpack: "center",
@@ -485,77 +513,24 @@ const WeatherWidget = () => {
       }),
     ],
     setup: (self) => {
-      const WEATHER_CACHE_PATH = WEATHER_CACHE_FOLDER + "/wttr.in.txt";
-      const updateWeatherForCity = (city) =>
-        Utils.execAsync(
-          `curl https://wttr.in/${city.replace(/ /g, "%20")}?format=j1`,
-        )
-          .then((output) => {
-            const weather = JSON.parse(output);
-            Utils.writeFile(
-              JSON.stringify(weather),
-              WEATHER_CACHE_PATH,
-            ).catch(print);
-            const weatherCode = weather.current_condition[0].weatherCode;
-            const weatherDesc =
-              weather.current_condition[0].weatherDesc[0].value;
-            const temperature =
-              weather.current_condition[0][
-                `temp_${userOptions.weather.preferredUnit}`
-              ];
-            const feelsLike =
-              weather.current_condition[0][
-                `FeelsLike${userOptions.weather.preferredUnit}`
-              ];
-            const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
-            updateWeatherDisplay(self, weather, weatherSymbol, temperature, feelsLike, weatherDesc);
-          })
-          .catch((err) => {
-            try {
-              const weather = JSON.parse(Utils.readFile(WEATHER_CACHE_PATH));
-              const weatherCode = weather.current_condition[0].weatherCode;
-              const weatherDesc =
-                weather.current_condition[0].weatherDesc[0].value;
-              const temperature =
-                weather.current_condition[0][
-                  `temp_${userOptions.weather.preferredUnit}`
-                ];
-              const feelsLike =
-                weather.current_condition[0][
-                  `FeelsLike${userOptions.weather.preferredUnit}`
-                ];
-              const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
-              updateWeatherDisplay(self, weather, weatherSymbol, temperature, feelsLike, weatherDesc);
-            } catch (err) {
-              print(err);
-            }
-          });
-
-      if (
-        userOptions.weather.city != "" &&
-        userOptions.weather.city != null
-      ) {
-        updateWeatherForCity(userOptions.weather.city.replace(/ /g, "%20"));
+      if (userOptions.weather.city != "" && userOptions.weather.city != null) {
+        fetchWeatherData(self, userOptions.weather.city.replace(/ /g, "%20"));
       } else {
         Utils.execAsync("curl ipinfo.io")
           .then((output) => JSON.parse(output)["city"].toLowerCase())
-          .then(updateWeatherForCity)
+          .then((city) => fetchWeatherData(self, city))
           .catch(print);
       }
 
       self.hook(Bluetooth, () => {
-        // Re-fetch weather data to update the display based on Bluetooth connection status
-        if (
-          userOptions.weather.city != "" &&
-          userOptions.weather.city != null
-        ) {
-          updateWeatherForCity(userOptions.weather.city.replace(/ /g, "%20"));
-        } else {
-          Utils.execAsync("curl ipinfo.io")
-            .then((output) => JSON.parse(output)["city"].toLowerCase())
-            .then(updateWeatherForCity)
-            .catch(print);
-        }
+        // Directly update the display without re-fetching weather data
+        const weather = JSON.parse(Utils.readFile(WEATHER_CACHE_FOLDER + "/wttr.in.txt"));
+        const weatherCode = weather.current_condition[0].weatherCode;
+        const weatherDesc = weather.current_condition[0].weatherDesc[0].value;
+        const temperature = weather.current_condition[0][`temp_${userOptions.weather.preferredUnit}`];
+        const feelsLike = weather.current_condition[0][`FeelsLike${userOptions.weather.preferredUnit}`];
+        const weatherSymbol = WEATHER_SYMBOL[WWO_CODE[weatherCode]];
+        updateWeatherDisplay(self, weather, weatherSymbol, temperature, feelsLike, weatherDesc);
       }, "notify::connected-devices");
     },
   });
